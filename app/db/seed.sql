4d247de5811bf43187d85dc1fbc85f6fb96236bc
DAZY <quentin.dazy@telecomnancy.eu>
2026-02-06 19:20:04 +0100

    modifs légères de la BD

--- app/db/seed.sql
+++ app/db/seed.sql
@@ -11,14 +11,14 @@ VALUES  ("Printemps"),
         ("Toute saison");
 
 INSERT OR IGNORE INTO ingredients(nom, id_saison) 
-VALUES  ("Pomme",(SELECT id_saison FROM saisons WHERE nom = "Automne")),
-        ("Poire",(SELECT id_saison FROM saisons WHERE nom = "Automne")),
-        ("Banane",(SELECT id_saison FROM saisons WHERE nom = "Toute saison")),
-        ("Kiwi vert",(SELECT id_saison FROM saisons pommeWHERE nom = "Toute saison")),
-        ("Orange",(SELECT id_saison FROM saisons WHERE nom = "Hiver")),
-        ("Pamplemousse",(SELECT id_saison FROM saisons WHERE nom = "Hiver")),
-        ("Pastèque",(SELECT id_saison FROM saisons WHERE nom = "Été")),
-        ("Melon",(SELECT id_saison FROM saisons WHERE nom = "Été"));
+VALUES  ("Pomme",(SELECT id FROM saisons WHERE nom = "Automne")),
+        ("Poire",(SELECT id FROM saisons WHERE nom = "Automne")),
+        ("Banane",(SELECT id FROM saisons WHERE nom = "Toute saison")),
+        ("Kiwi vert",(SELECT id FROM saisons WHERE nom = "Toute saison")),
+        ("Orange",(SELECT id FROM saisons WHERE nom = "Hiver")),
+        ("Pamplemousse",(SELECT id FROM saisons WHERE nom = "Hiver")),
+        ("Pastèque",(SELECT id FROM saisons WHERE nom = "Été")),
+        ("Melon",(SELECT id FROM saisons WHERE nom = "Été"));
 
 INSERT OR IGNORE INTO difficultes(nom)
 VALUES  ("Très Facile"),
@@ -28,6 +28,5 @@ VALUES  ("Très Facile"),
         ("Très Difficile");
 
 INSERT OR IGNORE INTO recettes(titre, temps, id_difficulte, id_auteur, recompense_xp)
-VALUES  ("Lasagnes","2","hash1",500,2,1,0),
-        ("Léonie","leonie.ferrier@telecomnancy.net","hash2",350,1,1,0);
+VALUES  ("Lasagnes","40",(SELECT id FROM difficultes WHERE nom = "Avancé"),(SELECT id FROM utilisateurs WHERE nom = "Quentin"),20);
 
