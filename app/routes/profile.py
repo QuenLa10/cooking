46fac5a91bfc34c0c275d9e3caa689f5819282b6
Leonie <leonie.ferrier@telecomnancy.net>
2026-02-11 17:28:20 +0100

    je sais pas

--- app/routes/profile.py
+++ app/routes/profile.py
@@ -1,10 +1,22 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 from app.routes.decorator import login_required, admin_required
+from app.database import get_db
 
 profile_bp = Blueprint("profile", __name__)
 
 # Afficher les informations personnelle
-@profile_bp.route("/profile/<int:id>")
+@profile_bp.route("/profile")
 @login_required
 def profile(id):
-    return render_template('profile.html')
+    identifiant = session.get('identifiant')
+    base = get_db()
+    cur = base.cursor()
+    cur.execute("SELECT * FROM utilisateurs WHERE identifiant = ?", (identifiant,))
+    info_utilisateur = cur.fetchone()
+    return render_template('profile.html', info_utilisateur = info_utilisateur)
+
+# Changer de mot de passe
+
+# Pour les admin : accorder le role admin a d'autres personnes
+
+
